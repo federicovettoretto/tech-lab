a9c8e7e00fc3ab3f76623222544593b9b893c7ae
Federico Vettoretto <federico.vettoretto@maize.io>
2025-10-23 10:16:07 +0200

    Initial Commit

---
+++ Tech-lab/Tech_labApp.swift
@@ -0,0 +1,18 @@
+//
+//  Tech_labApp.swift
+//  Tech-lab
+//
+//  Created by Federico Vettoretto on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Tech_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
